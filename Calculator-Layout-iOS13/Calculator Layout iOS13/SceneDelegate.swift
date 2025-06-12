ee5b2b4161fcbe570450bedf53706a99ff72287f
Taneem Ahmed <62951544+TaneemAhmed2013102@users.noreply.github.com>
2025-06-12 08:58:16 +0600

    Update SceneDelegate.swift

--- Calculator-Layout-iOS13/Calculator Layout iOS13/SceneDelegate.swift
+++ Calculator-Layout-iOS13/Calculator Layout iOS13/SceneDelegate.swift
@@ -2,9 +2,8 @@
 //  SceneDelegate.swift
 //  Calculator Layout iOS13
 //
-//  Created by Angela Yu on 01/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
 //
+//  Created by Taneem on 26/09/2024.
 
 import UIKit
 
