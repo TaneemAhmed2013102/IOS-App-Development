8b666fb898cb98cdd91aa55332531ee013c6ecae
Taneem Ahmed <62951544+TaneemAhmed2013102@users.noreply.github.com>
2025-06-12 08:55:17 +0600

    Update AppDelegate.swift

--- Dicee/Dicee/AppDelegate.swift
+++ Dicee/Dicee/AppDelegate.swift
@@ -2,8 +2,7 @@
 //  AppDelegate.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Taneem on 22/09/2024.
 //
 
 import UIKit
