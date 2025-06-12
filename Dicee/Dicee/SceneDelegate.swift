dcfcecd8406dc7fc3f47e8a4aa89af397414fbdb
Taneem Ahmed <62951544+TaneemAhmed2013102@users.noreply.github.com>
2025-06-12 08:55:01 +0600

    Update SceneDelegate.swift

--- Dicee/Dicee/SceneDelegate.swift
+++ Dicee/Dicee/SceneDelegate.swift
@@ -2,8 +2,7 @@
 //  SceneDelegate.swift
 //  Test
 //
-//  Created by Angela Yu on 13/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Taneem on 22/09/2024.
 //
 
 import UIKit
