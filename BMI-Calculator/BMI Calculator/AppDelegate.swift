8e3ea66089221b5d9907beaadd0cba6de8f9e462
Taneem Ahmed <62951544+TaneemAhmed2013102@users.noreply.github.com>
2025-06-12 09:16:38 +0600

    Update AppDelegate.swift

--- BMI-Calculator/BMI Calculator/AppDelegate.swift
+++ BMI-Calculator/BMI Calculator/AppDelegate.swift
@@ -2,9 +2,7 @@
 //  AppDelegate.swift
 //  BMI Calculator
 //
-//  Created by Angela Yu on 21/08/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
+//  Created by Taneem Ahmed on 11/10/24.
 
 import UIKit
 
